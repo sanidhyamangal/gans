b44b63a55e3939876bf553734b5df4232614351d
Sanidhya <mangalsanidhya19@gmail.com>
2020-12-26 12:17:35 +0530

    update: removed assert checks for the base mixin saver

--- checkpoint.py
+++ checkpoint.py
@@ -14,22 +14,6 @@ class BaseCheckpointSaverMixin:
     """
     def save_checkpoint(self,
                         checkpoint_dir: str = "./training_checkpoint") -> None:
-
-        assert self.generator is None, (
-            "%s must have generator for creating a checkpoint" %
-            self.__class__.__name__)
-
-        assert self.discriminator is None, (
-            "%s must have discriminator object for creating a checkpoint" %
-            self.__class__.__name__)
-
-        assert self.discriminator_optimizer is None, (
-            "%s must have discriminator_optimizer object for creating a checkpoint"
-            % self.__class__.__name__)
-
-        assert self.generator_optimizer is None, (
-            "%s must have generator_optimizer object for creating a checkpoint"
-            % self.__class__.__name__)
         
         checkpoint_prefix = os.path.join(checkpoint_dir,"ckpt")
         checkpoint = tf.train.Checkpoint(generator=self.generator, discriminator=self.discriminator, discriminator_optimizer=self.discriminator_optimizer, generator_optimizer=self.generator_optimizer)
